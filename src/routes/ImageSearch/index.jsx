bf61569aed454113b42abac5e42587ba0cb84465
tharunoptimus <tharunoptimus@outlook.com>
2023-09-08 20:04:48 +0530

    Base of image search works

--- src/routes/ImageSearch/index.jsx
+++ src/routes/ImageSearch/index.jsx
@@ -1,66 +1,49 @@
-import React, { useRef, useEffect, useState } from 'react';
-import Webcam from 'react-webcam';
+import React, { useRef, useEffect, useState } from "react"
+import Webcam from "react-webcam"
+import * as tf from "@tensorflow/tfjs"
+import * as mobilenet from "@tensorflow-models/mobilenet"
 
 export default function ImageSearch() {
-  const webcamRef = useRef(null);
-  const [worker, setWorker] = useState(null);
+	const webcamRef = useRef(null)
+	const [model, setModel] = useState(null)
 
-  useEffect(() => {
-    const tfWorker = new Worker(new URL('./tfWorker.mjs', import.meta.url), { type: 'module' });
+	useEffect(() => {
+		async function loadModel() {
+			const model = await mobilenet.load()
+			setModel(model)
+		}
+		loadModel()
+	}, [])
 
-    tfWorker.onmessage = (event) => {
-      const { type, predictions } = event.data;
+	const capture = async () => {
+		const canvas = document.createElement("canvas")
+		canvas.width = webcamRef.current.video.videoWidth
+		canvas.height = webcamRef.current.video.videoHeight
+		const ctx = canvas.getContext("2d")
+		ctx.drawImage(webcamRef.current.video, 0, 0)
+		const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+		const rawPixelData = Array.from(new Uint8Array(imageData.data))
 
-      switch (type) {
-        case 'modelLoaded':
-          console.log('Model loaded in worker!');
-          break;
+		if (model) {
+			const tensor = tf.browser
+				.fromPixels(canvas)
+				.resizeNearestNeighbor([224, 224])
+				.toFloat()
+				.expandDims()
+			const predictions = await model.classify(tensor)
+			console.log(predictions)
+		}
+	}
 
-        case 'predictions':
-          console.log(predictions);
-          break;
-
-        default:
-          console.error('Unknown message type from worker');
-      }
-    };
-
-    tfWorker.postMessage({ type: 'loadModel' });
-    setWorker(tfWorker);
-
-    // Cleanup the worker when the component unmounts
-    return () => {
-      tfWorker.terminate();
-    };
-  }, []);
-
-  const capture = () => {
-    const canvas = document.createElement('canvas');
-    canvas.width = webcamRef.current.video.videoWidth;
-    canvas.height = webcamRef.current.video.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(webcamRef.current.video, 0, 0);
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const rawPixelData = Array.from(new Uint8Array(imageData.data));
-
-    worker && worker.postMessage({
-      type: 'classify',
-      width: canvas.width,
-      height: canvas.height,
-      rawPixelData
-    });
-};
-
-
-  return (
-    <>
-      <Webcam
-        audio={false}
-        ref={webcamRef}
-        screenshotFormat="image/jpeg"
-        videoConstraints={{ facingMode: "user" }}
-      />
-      <button onClick={capture}>Classify Image</button>
-    </>
-  );
+	return (
+		<>
+			<Webcam
+				audio={false}
+				ref={webcamRef}
+				screenshotFormat="image/jpeg"
+				videoConstraints={{ facingMode: "environment" }}
+			/>
+			<button onClick={capture}>Classify Image</button>
+		</>
+	)
 }
