6efd510a164cc6bac8692569b0c05ba8cba165a0
tharunoptimus <tharunoptimus@outlook.com>
2023-09-08 14:31:14 +0530

    Added routes to `main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import Home from "./routes/Home"
+import ImageSearch from "./routes/ImageSearch"
+import Search from "./routes/Search"
+
+
+import NotFoundError from "./routes/NothingHere"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<BrowserRouter>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="search" element={<Search />} />
+      <Route path="imageSearch" element={<ImageSearch />} />
+			
+			<Route path="*" element={<NotFoundError />} />
+		</Routes>
+	</BrowserRouter>
 )
