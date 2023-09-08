bde37173fe6e6461c05eb3c3ac533ff64436c510
tharunoptimus <tharunoptimus@outlook.com>
2023-09-08 20:04:37 +0530

    Added tfjs as external dependencies during build

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@tensorflow/tfjs', '@tensorflow-models/mobilenet']
+    },
+    // This is used to prevent warning during development
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
 })
