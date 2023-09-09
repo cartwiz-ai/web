336f25946aa40383e11ff6b69365c20a5f11142a
tharunoptimus <tharunoptimus@outlook.com>
2023-09-09 12:01:29 +0530

    Man is working

--- src/routes/TryOn/index.jsx
+++ src/routes/TryOn/index.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react"
 import ProductItem from "../../components/ProductItem"
 import PageTitle from "../../components/PageTitle"
 import styles from "./TryOn.module.css"
+import man from "/man.jpg"
 
 
 function TryOn() {
@@ -21,6 +22,8 @@ function TryOn() {
 	let [image, setImage] = useState(null)
 	let [showImageUpload, setShowImageUpload] = useState(true)
 	let [showTryOnButton, setShowTryOnButton] = useState(false)
+	let [showGenerating, setShowGenerating] = useState(false)
+	let [showProduct, setShowProduct] = useState(true)
 
 	useEffect(()=> {
 		
@@ -76,17 +79,26 @@ function TryOn() {
 		}
 	}
 
-	const handleDragOver = (event) => {
-		event.preventDefault()
+	const handleTryOn = () => {
+		setShowGenerating(true)
+		setShowTryOnButton(false)
+		setShowImageUpload(false)
+		setShowProduct(false)
+		setTimeout(() => {
+			setShowGenerating(false)
+			setImage("/man.jpg")
+		}, 3000)
 	}
 
 	return (
 		ready ? (
 			<div className={styles.tryOnWrapper}>
 				<PageTitle title="Try On" />
-				<ProductItem
+				{
+					showProduct ? <ProductItem
 					product={{productDisplayName, baseColour, contentId, _id}}
-				/>
+				/> : null
+				}
 				{showImageUpload && (
 					<div className={styles.imageUploadWrapper}>
 						<label htmlFor="imageUpload">Upload Image</label>
@@ -99,7 +111,6 @@ function TryOn() {
 						<div
 							className={styles.dropRegion}
 							onDrop={handleDrop}
-							onDragOver={handleDragOver}
 							onClick={() => document.getElementById('imageUpload').click()}
 						>
 							Drop Image Here
@@ -108,11 +119,14 @@ function TryOn() {
 				)}
 				{image && (
 					<div className={styles.imagePreviewWrapper}>
-						<img src={image} alt="User uploaded" className={styles.userUploadedImage} />
+						<img src={image} alt="User uploaded" className={styles.userUploadedImage} height={260} />
 					</div>
 				)}
 				{showTryOnButton && (
-					<button className={styles.tryOnButton}>TryOn Now</button>
+					<button className={styles.tryOnButton} onClick={handleTryOn}>TryOn Now</button>
+				)}
+				{showGenerating && (
+					<Generating />
 				)}
 			</div>
 		) : <Loading />
@@ -126,6 +140,14 @@ function Loading() {
 	return (
 		<div className={styles.loadingWrapper}>
 			<h1>Please wait while we make arrangements to Try on...</h1>
+		</div>
+	)
+}
+
+function Generating() {
+	return (
+		<div className={styles.loadingWrapper}>
+			<h1>Generating Try On... Please Wait...</h1>
 		</div>
 	)
 }
