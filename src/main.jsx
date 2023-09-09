0ebbabcba70cac1a6090e0d10ffc310815f4ff1a
tharunoptimus <tharunoptimus@outlook.com>
2023-09-09 13:07:10 +0530

    Added regular.woff2 font everywhere

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import "./index.css"
 import Home from "./routes/Home"
 import ImageSearch from "./routes/ImageSearch"
 import Search from "./routes/Search"
