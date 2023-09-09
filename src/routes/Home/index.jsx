00a799d812da06c45208506ef698cbc10430a178
tharunoptimus <tharunoptimus@outlook.com>
2023-09-09 10:54:19 +0530

    Added Home Page suggestions

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,7 +1,60 @@
+import { useState, useEffect } from "react"
 import SearchBar from "./SearchBar"
+import styles from "./Home.module.css"
+import ProductItem from "../../components/ProductItem"
 
 export default function Home() {
-	return <>
-		<SearchBar />
-	</>
+	return (
+		<>
+			<SearchBar />
+			<Recommendations />
+		</>
+	)
+}
+
+function Category({ categoryName, items }) {
+	return (
+		<div className={styles.categoryWrapper}>
+			<h2 className={styles.categoryName}>{categoryName}</h2>
+			<div className={styles.productsHolder}>
+				{items.map((item, index) => (
+					<ProductItem product={{ ...item }} key={index} />
+				))}
+			</div>
+		</div>
+	)
+}
+
+function Recommendations() {
+	const URL_TO_FETCH = `http://localhost:4003/api/product/home`
+
+	const [categories, setCategories] = useState([])
+	let [isLoading, setIsLoading] = useState(true)
+
+	useEffect(() => {
+		async function main() {
+			const request = await fetch(URL_TO_FETCH)
+			const response = await request.json()
+
+			console.log(response)
+
+			setCategories(response)
+			setIsLoading(false)
+		}
+		main()
+	}, [])
+
+	return isLoading ? (
+		<div>Loading...</div>
+	) : (
+		<>
+			{categories.map((category, index) => (
+				<Category
+					categoryName={category.categoryName}
+					items={category.products}
+					key={index}
+				/>
+			))}
+		</>
+	)
 }
